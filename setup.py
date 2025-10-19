3523719a16c72ebaa5fa6729addb35aa33bb91cf
Siddharth Ajith <siddharth@skellam.ai>
2025-10-20 00:12:39 +0530

    create versions, custom exception

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="document_portal",
+    name="deepdoc-ai",
     author="Siddharth Ajith",
     version="0.1",
     packages=find_packages(),
